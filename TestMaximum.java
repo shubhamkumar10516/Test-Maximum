4905e652508958f570d04993eed3ebed9f0db9cb
shubham <shubhamkumar10516@gmail.com>
2020-10-08 21:36:54 +0530

    [Shubham]ADDED: UC2 finding maximum among three doubles and testing it

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,9 +1,19 @@
 package com.bridgeLabz.testMaximum;
 
 public class TestMaximum {
+	//find max among integers
 	public static int findMaxOfThree(Integer a, Integer b, Integer c) {
 
 		Integer max = a;
+		max = b.compareTo(max) > 0 ? b : max;
+		max = c.compareTo(max) > 0 ? c : max;
+		return max;
+	}
+	
+	//find max among integers
+	public static double findMaxOfThree(Double a, Double b, Double c) {
+
+		Double max = a;
 		max = b.compareTo(max) > 0 ? b : max;
 		max = c.compareTo(max) > 0 ? c : max;
 		return max;
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -20,4 +20,19 @@ public class TestMaximumTest {
 	public void findMaxOfThreeTc3() {
 		assertEquals(900, TestMaximum.findMaxOfThree(400, 200, 900));
 	}
+	
+	@Test
+	public void findMaxOfThreeTc4() {
+		assertTrue(300.56 == TestMaximum.findMaxOfThree(300.56, 100.92, 200.6));
+	}
+	
+	@Test
+	public void findMaxOfThreeTc5() {
+		assertTrue(700.36 == TestMaximum.findMaxOfThree(400.98, 700.36, 200.99));
+	}
+	
+	@Test
+	public void findMaxOfThreeTc6() {
+		assertTrue(900.23 == TestMaximum.findMaxOfThree(400.5, 200.8, 900.23));
+	}
 }
