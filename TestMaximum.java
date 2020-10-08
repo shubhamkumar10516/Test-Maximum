5336691e2cbcabdb1492b8d944ca458c36f6994b
shubham <shubhamkumar10516@gmail.com>
2020-10-09 01:19:04 +0530

    [Shubham]REFACTORED: generic class added with constructor and testMax method in same UC3 file

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,12 +1,27 @@
 package com.bridgeLabz.testMaximum;
 
 public class TestMaximum {
-	// find max among integers
-	public static<T extends Comparable<T>> T findMaxOfThree(T a, T b, T c) {
 
+	// find max among integers
+	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
 		T max = a;
 		max = b.compareTo(max) > 0 ? b : max;
 		max = c.compareTo(max) > 0 ? c : max;
 		return max;
+	}
+}
+
+class Generic<T extends Comparable<T>> {
+
+	T v1, v2, v3;
+    //parameterized constructor
+	public Generic(T a, T b, T c) {
+		v1 = a;
+		v2 = b;
+		v3 = c;
+	}
+    //finding maximum method
+	public T testMaximum() {
+		return TestMaximum.testMaximum(v1, v2, v3);
 	}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -7,47 +7,65 @@ import org.junit.Test;
 public class TestMaximumTest {
 
 	@Test
-	public void findMaxOfThreeTc1() {
-		assertTrue(300 == TestMaximum.findMaxOfThree(300, 100, 200));
+	public void testMaximumTc1() {
+		assertTrue(300 == TestMaximum.testMaximum(300, 100, 200));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc2() {
-		assertTrue(700 == TestMaximum.findMaxOfThree(400, 700, 200));
+	public void testMaximumTc2() {
+		assertTrue(700 == TestMaximum.testMaximum(400, 700, 200));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc3() {
-		assertTrue(900 == TestMaximum.findMaxOfThree(400, 200, 900));
+	public void testMaximumTc3() {
+		assertTrue(900 == TestMaximum.testMaximum(400, 200, 900));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc4() {
-		assertTrue(300.56 == TestMaximum.findMaxOfThree(300.56, 100.92, 200.6));
+	public void testMaximumTc4() {
+		assertTrue(300.56 == TestMaximum.testMaximum(300.56, 100.92, 200.6));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc5() {
-		assertTrue(700.36 == TestMaximum.findMaxOfThree(400.98, 700.36, 200.99));
+	public void testMaximumTc5() {
+		assertTrue(700.36 == TestMaximum.testMaximum(400.98, 700.36, 200.99));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc6() {
-		assertTrue(900.23 == TestMaximum.findMaxOfThree(400.5, 200.8, 900.23));
+	public void testMaximumTc6() {
+		assertTrue(900.23 == TestMaximum.testMaximum(400.5, 200.8, 900.23));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc7() {
-		assertTrue("peach".equals(TestMaximum.findMaxOfThree("apple", "peach", "banana")));
+	public void testMaximumTc7() {
+		assertTrue("peach".equals(TestMaximum.testMaximum("apple", "peach", "banana")));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc8() {
-		assertTrue("peach".equals(TestMaximum.findMaxOfThree("peach", "apple", "banana")));
+	public void testMaximumTc8() {
+		assertTrue("peach".equals(TestMaximum.testMaximum("peach", "apple", "banana")));
 	}
 	
 	@Test
-	public void findMaxOfThreeTc9() {
-		assertTrue("peach".equals(TestMaximum.findMaxOfThree("peach", "banana", "apple")));
+	public void testMaximumTc9() {
+		assertTrue("peach".equals(TestMaximum.testMaximum("peach", "banana", "apple")));
+	}
+	
+	@Test
+	public void testMaximumTc10() {
+		Generic<Integer> gnObj = new Generic<Integer>(4,3,1);
+		assertTrue(4 == (gnObj.testMaximum()));
+	}
+	
+	@Test
+	public void testMaximumTc11() {
+		Generic<Double> gnObj = new Generic<Double>(4.1,4.3,1.9);
+		assertTrue(4.3 == (gnObj.testMaximum()));
+	}
+	
+	@Test
+	public void testMaximumTc12() {
+		Generic<String> gnObj = new Generic<String>("apple","banana","pear");
+		assertTrue("pear".equals(gnObj.testMaximum()));
 	}
 }
