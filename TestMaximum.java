47d7a3eb0e4f8c50dd7df5f69706a5a4a0ba8204
shubhamkumar10516 <33356820+shubhamkumar10516@users.noreply.github.com>
2020-10-10 00:51:09 +0530

    [Shubham]REFACTORED: options added over list

--- TestMaximum.java
+++ TestMaximum.java
@@ -5,10 +5,12 @@ import java.util.*;
 public class TestMaximum {
 
 	// find max among integers
-	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c, T... d) {
 		T max = a;
 		max = b.compareTo(max) > 0 ? b : max;
 		max = c.compareTo(max) > 0 ? c : max;
+		for (T val : d)
+			max = val.compareTo(max) > 0 ? val : max;
 		return max;
 	}
 
