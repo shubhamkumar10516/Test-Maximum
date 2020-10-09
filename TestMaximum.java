6ca141177de2df7aa87468ad46335f8b9002701b
shubham <shubhamkumar10516@gmail.com>
2020-10-09 22:38:18 +0530

    [Shubham]REFACTORED: Creating single generic method of all data types

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,30 +1,12 @@
 package com.bridgeLabz.testMaximum;
 
 public class TestMaximum {
-	//find max among integers
-	public static int findMaxOfThree(Integer a, Integer b, Integer c) {
-
-		Integer max = a;
+	
+	// find max among integers
+	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+		T max = a;
 		max = b.compareTo(max) > 0 ? b : max;
 		max = c.compareTo(max) > 0 ? c : max;
 		return max;
 	}
-	
-	//find max among integers
-	public static double findMaxOfThree(Double a, Double b, Double c) {
-
-		Double max = a;
-		max = b.compareTo(max) > 0 ? b : max;
-		max = c.compareTo(max) > 0 ? c : max;
-		return max;
-	}
-	
-	//find max among Strings
-		public static String findMaxOfThree(String a, String b, String c) {
-
-			String max = a;
-			max = b.compareTo(max) > 0 ? b : max;
-			max = c.compareTo(max) > 0 ? c : max;
-			return max;
-		}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -8,17 +8,17 @@ public class TestMaximumTest {
 
 	@Test
 	public void findMaxOfThreeTc1() {
-		assertEquals(300, TestMaximum.findMaxOfThree(300, 100, 200));
+		assertTrue(300 == TestMaximum.findMaxOfThree(300, 100, 200));
 	}
 	
 	@Test
 	public void findMaxOfThreeTc2() {
-		assertEquals(700, TestMaximum.findMaxOfThree(400, 700, 200));
+		assertTrue(700 == TestMaximum.findMaxOfThree(400, 700, 200));
 	}
 	
 	@Test
 	public void findMaxOfThreeTc3() {
-		assertEquals(900, TestMaximum.findMaxOfThree(400, 200, 900));
+		assertTrue(900 == TestMaximum.findMaxOfThree(400, 200, 900));
 	}
 	
 	@Test
